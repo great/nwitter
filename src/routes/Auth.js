f16e9bc6304e69f31b7dc3fc90bd1c2052b8da76
Great <ikim@path.how>
2022-07-28 17:39:07 +0900

    04-4 log in, log out

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
     const onChange = (event) => {
         const {
@@ -30,9 +31,11 @@ const Auth = () => {
             }
             console.log(data);
         } catch (error) {
-            console.log(error);
+            setError(error.message);
         }
     }
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
 
     return (
         <div>
@@ -40,7 +43,9 @@ const Auth = () => {
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Log In"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
