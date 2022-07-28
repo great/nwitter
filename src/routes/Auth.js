4bbbff0452d44bd111517c328b92aaad0801b028
Great <ikim@path.how>
2022-07-28 17:50:42 +0900

    04-5 social login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import { useState } from "react";
 
 const Auth = () => {
@@ -16,7 +16,7 @@ const Auth = () => {
         } else if (name === "password") {
             setPassword(value);
         }
-    }
+    };
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -33,9 +33,23 @@ const Auth = () => {
         } catch (error) {
             setError(error.message);
         }
-    }
+    };
 
     const toggleAccount = () => setNewAccount((prev) => !prev);
+
+    const onSocialClick = async (event) => {
+        const {
+            target: { name },
+        } = event;
+        let provider;
+        if (name === "google") {
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if (name === "github") {
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
+    };
 
     return (
         <div>
@@ -47,8 +61,8 @@ const Auth = () => {
             </form>
             <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Log In"}</span>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with Github</button>
             </div>
         </div>
     );
