4a4640e6a00c3074b6c3a65f401277ee9ae0a0b9
Great <ikim@path.how>
2022-08-03 16:30:45 +0900

    profile in progress

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,28 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";    // https://stackoverflow.com/questions/62861269/attempted-import-error-usehistory-is-not-exported-from-react-router-dom
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const navigate = useNavigate();
 
     const onLogOutClick = () => {
         authService.signOut();
         navigate("/");
     };
+
+    const getMyNweets = async () => {
+        const nweets = await dbService
+        .collection("nweets")
+        .where("creatorId", "==", userObj.uid)
+        .orderBy("createdAt", "asc")
+        .get();
+
+        console.log(nweets.docs.map((doc) => doc.data()));
+    };
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
 
     return (
         <>
