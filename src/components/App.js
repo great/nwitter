8bcac95b197afe581a1e199e89ea3c13bab3a0b8
Great <ikim@path.how>
2022-08-03 13:47:48 +0900

    end of chapter 5

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import { authService } from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   // setInterval(() => console.log(authService.currentUser), 2000);
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(user);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -21,7 +23,11 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "initializing..."
+      )}
       
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
