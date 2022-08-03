c8038fe8b901e90ed3d4e1750e7eed8db1d8ca79
Great <ikim@path.how>
2022-08-03 17:35:56 +0900

    end of chapter 7

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,31 +1,37 @@
 import { authService, dbService } from "fbase";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";    // https://stackoverflow.com/questions/62861269/attempted-import-error-usehistory-is-not-exported-from-react-router-dom
 
-const Profile = ({ userObj }) => {
+const Profile = ({ userObj, refreshUser }) => {
     const navigate = useNavigate();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
 
     const onLogOutClick = () => {
         authService.signOut();
         navigate("/");
     };
 
-    const getMyNweets = async () => {
-        const nweets = await dbService
-        .collection("nweets")
-        .where("creatorId", "==", userObj.uid)
-        .orderBy("createdAt", "asc")
-        .get();
-
-        console.log(nweets.docs.map((doc) => doc.data()));
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
     };
 
-    useEffect(() => {
-        getMyNweets();
-    }, []);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({ displayName: newDisplayName });
+            refreshUser();
+        }
+    };
 
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} type="text" placeholder="Display Name" value={newDisplayName} />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
